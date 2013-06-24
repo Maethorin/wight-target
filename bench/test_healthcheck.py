bdccef36844e31e4bbfc94d768d4812e061fc5dc
Márcio Duarte <maethorin@gmail.com>
2013-06-24 15:17:54 -0300

    Removing home test.

--- bench/test_healthcheck.py
+++ bench/test_healthcheck.py
@@ -18,7 +18,10 @@ class HealthCheckTest(FunkLoadTestCase):
         self.server_url = '%s/healthcheck' % (self.conf_get('main', 'url').rstrip('/'),)
 
     def test_healthcheck(self):
+
         self.get(self.server_url, description='Get url')
 
 if __name__ == '__main__':
     unittest.main()
+
+
